b9ab2b05bd9e50dbacb8bbbcd2d4f5dd2989ab7d
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-03-07 18:51:59 +0000

    V01

--- Clases/React/01 - 06 de Marzo/practica1/src/Componets/Comprobar.jsx
+++ Clases/React/01 - 06 de Marzo/practica1/src/Componets/Comprobar.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
-// import App from '../App';
-import axios from 'axios';
+import axios from 'axios'
+
 
 export const Comprobar = () => {
-    
-    const listar = async () => {
-        try {
-            const datos = await axios.get(
-                "https://6400981f63e89b0913b297b5.mockapi.io/api/v1/clientes"
-            );
-            return datos;
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    listar()
-        .then((datos) => {
-                <ul>
-                {datos.map((enlace, index) => (
-                    <li>{enlace[index]}</li>
-                ))}
-                </ul> 
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  const listar = async () => {
+    try {
+      const datos = await axios.get(
+        "https://6400981f63e89b0913b297b5.mockapi.io/api/v1/clientes"
+      );
+      return datos;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  listar()
+    .then((datos) => {
+      let lista = " ";
+      
+      for (let i = 0; i < datos.data.length; i++) {
+        lista +=
+          <tr key={datos.data[i].id}>
+            <td>{datos.data[i].id}</td>
+            <td>{datos.data[i].nombre}</td></tr>
+      }
+
+
+    })
 }
+
+export default Comprobar
