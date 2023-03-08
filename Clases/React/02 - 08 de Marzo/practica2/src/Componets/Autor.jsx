a2cf59c3c099d6f1d88b4c363cc426fd98e32f48
Gabriel <entropia7@gmail.com>
2023-03-08 11:15:40 +0000

    V01

--- Clases/React/02 - 08 de Marzo/practica2/src/Componets/Autor.jsx
+++ Clases/React/02 - 08 de Marzo/practica2/src/Componets/Autor.jsx
@@ -4,9 +4,9 @@ import Blog from './Blog'
 const Autor = ({autor, fecha, mensaje}) => {
   return (
       <div>
-          <h3>Autor</h3>
+          
           <ul>
-              <h3>Autor: {autor} Mensaje:{mensaje} Fecha:{fecha }</h3>
+              <h3>{autor}{mensaje}{fecha }</h3>
           </ul>  
       </div>
   )
