6f58bc8f6da02584de18c1e74ba1fedc45a2a994
Gabriel <gabrielmedinatorres@hotmail.com>
2023-03-09 21:44:31 +0000

    V0.1

---
+++ Practicas propias/Belinda/belinda/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import "./componets/Cabecera.jsx";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ Practicas propias/Belinda/belinda/src/componets/Cabecera.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Cabecera.css'
+
+const header = () => {
+  return (
+    <div className='cabecera'><h1>Cabecera</h1></div>
+  )
+}
+
+export default header
