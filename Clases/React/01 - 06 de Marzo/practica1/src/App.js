5a241e16c94407068ef18745d67cb006c818fd67
Gabriel <entropia7@gmail.com>
2023-03-09 14:06:50 +0000

    V02

--- Clases/React/01 - 06 de Marzo/practica1/src/App.js
+++ Clases/React/01 - 06 de Marzo/practica1/src/App.js
@@ -3,16 +3,33 @@ import { v4 as uuidv4 } from "uuid";
 
 function Example() {
   const [count, incrementa] = useState(0);
+  const [ratonEncima, setRatonEncima] = useState(false);
   const nuevoObjeto = {
     id: uuidv4(),
     nombre: "Juan",
   };
   console.log(nuevoObjeto.id);
+  const incrementar = () => {
+    incrementa(count + 1);
+  };
+  const gestionRatonSobre = () => {
+    setRatonEncima(true);
+  };
+  const gestionRatonFuera = () => {
+    setRatonEncima(false);
+  };
 
   return (
     <div>
       <p>Clicado {count} veces</p>
-      <button onClick={() => incrementa(count + 1)}>Clica aqui</button>
+      <button
+        style={{ backgroundColor: ratonEncima ? "red" : "blue" }}
+        onClick={incrementar}
+        onMouseOver={gestionRatonFuera}
+        onMouseOut={gestionRatonSobre}
+      >
+        Clica aqui
+      </button>
     </div>
   );
 }
